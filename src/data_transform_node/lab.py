0e4b88c7517fb673c15a1f9b220540d7c998ac7b
hibana2077 <hibana2077@gmail.com>
2024-03-11 17:52:55 +0800

    add huggingface transformers to the project

--- src/data_transform_node/lab.py
+++ src/data_transform_node/lab.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-03-08 12:27:47
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-03-11 08:59:53
+LastEditTime: 2024-03-11 17:52:47
 FilePath: \plant_knowledge_pipepline\src\data_transform_node\lab.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -18,10 +18,20 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from pprint import pprint
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from time import time
+
+access_token = "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"
+
+hf_model = HuggingFacePipeline.from_model_id(
+    model_id="abacaj/phi-2-super",
+    task="text-generation",
+    device=0,  # replace with device_map="auto" to use the accelerate library.
+    pipeline_kwargs={"max_new_tokens": 2000}
+)
 
 model = ChatGroq(temperature=0, groq_api_key="", model_name="mixtral-8x7b-32768")
 model2 = ChatOpenAI(temperature=0, openai_api_key="", model_name="gpt-3.5-turbo-16k")
-model3 = ChatGroq(temperature=0, groq_api_key="", model_name="llama2-70b-4096")
 
 # Define your desired data structure.
 class Property(BaseModel):
@@ -60,11 +70,13 @@ prompt = PromptTemplate(
 
 chain = prompt | model | parser
 chain2 = prompt | model2 | parser
-chain3 = prompt | model3 | parser
+chain3 = prompt | hf_model | parser
 
 print(f"Groq(mistral) output:")
 pprint(chain.invoke({"query": query}))
 print(f"Open AI(gpt-3.5-turbo-16k) output:")
 pprint(chain2.invoke({"query": query}))
-print(f"Groq(llama) output:")
+print(f"{hf_model.model_id} output:")
+s = time()
 pprint(chain3.invoke({"query": query}))
+print(f"Time taken: {time() - s}")
