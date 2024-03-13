8b84db402ae3462ce3861a1100d7b1f419ef1729
hibana2077 <hibana2077@gmail.com>
2024-03-13 13:35:39 +0800

    Update TEST.py with new code

--- src/data_transform_node/TEST.py
+++ src/data_transform_node/TEST.py
@@ -1,53 +1,71 @@
 '''
 Author: hibana2077 hibana2077@gmail.com
-Date: 2024-03-08 10:52:41
+Date: 2024-03-08 12:27:47
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-03-11 09:00:11
-FilePath: \plant_knowledge_pipepline\src\data_transform_node\TEST.py
+LastEditTime: 2024-03-12 16:15:13
+FilePath: \plant_knowledge_pipepline\src\data_transform_node\lab.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_groq import ChatGroq
-# chat = ChatGroq(temperature=0, groq_api_key="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", model_name="mixtral-8x7b-32768")
-
-# system = "You are a helpful assistant."
-# human = "{text}"
-# prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
-
-# chain = prompt | chat
-# out = chain.invoke({"text": "Explain the importance of low latency LLMs."})
-# out = chat.invoke("Explain the importance of low latency LLMs.")
-# print(out)
-
-
-
-from typing import Optional
-
-from langchain.chains import create_structured_output_runnable
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.pydantic_v1 import BaseModel, Field
-
-class Dog(BaseModel):
-    '''Identifying information about a dog.'''
-
-    name: str = Field(..., description="The dog's name")
-    color: str = Field(..., description="The dog's color")
-    fav_food: Optional[str] = Field(None, description="The dog's favorite food")
-
-llm = ChatGroq(temperature=0, groq_api_key="", model_name="mixtral-8x7b-32768")
-structured_llm = create_structured_output_runnable(Dog, llm, mode="openai-functions")
-system = '''Extract information about any dogs mentioned in the user input.'''
-prompt = ChatPromptTemplate.from_messages(
-    [("system", system), ("human", "{input}"),]
+from typing import List, Dict, Any, Optional
+from langchain_community.graphs.graph_document import (
+    Node as BaseNode,
+    Relationship as BaseRelationship,
+    GraphDocument,
 )
-chain = prompt | structured_llm
-print(structured_llm.invoke("Harry was a chubby brown beagle who loved chicken"))
-# print(chain._lc_kwargs.keys())
-# print(chain._lc_kwargs['middle'][0].kwargs)
-# chain._lc_kwargs['middle'][0].kwargs.pop('functions')
-# chain._lc_kwargs['middle'][0].kwargs.pop('function_call')
-# print("======")
-# print(chain._lc_kwargs['middle'][0].kwargs)
-# out = chain.invoke({"input": "Harry was a chubby brown beagle who loved chicken"})
-# print(out)
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+from pprint import pprint
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from time import time
+
+#mistralai/Mistral-7B-Instruct-v0.2
+
+hf_model = HuggingFacePipeline.from_model_id(
+    model_id="mistralai/Mistral-7B-Instruct-v0.2",
+    task="text-generation",
+    device=0,  # replace with device_map="auto" to use the accelerate library.
+    pipeline_kwargs={"max_new_tokens": 2000}
+)
+
+# Define your desired data structure.
+class Property(BaseModel):
+  """A single property consisting of key and value"""
+  key: str = Field(..., description="key")
+  value: str = Field(..., description="value")
+
+class Node(BaseNode):
+    properties: Optional[List[Property]] = Field(
+        None, description="List of node properties")
+
+class Relationship(BaseRelationship):
+    properties: Optional[List[Property]] = Field(
+        None, description="List of relationship properties"
+    )
+
+class KnowledgeGraph(BaseModel):
+    """Generate a knowledge graph with entities and relationships."""
+    nodes: List[Node] = Field(
+        ..., description="List of nodes in the knowledge graph")
+    rels: List[Relationship] = Field(
+        ..., description="List of relationships in the knowledge graph"
+    )
+
+# And a query intented to prompt a language model to populate the data structure.
+query = "Harry was a chubby brown beagle who loved chicken."
+
+# Set up a parser + inject instructions into the prompt template.
+parser = JsonOutputParser(pydantic_object=KnowledgeGraph)
+
+prompt = PromptTemplate(
+    template="Answer the user query.\n{format_instructions}\n{query}\n",
+    input_variables=["query"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
+
+chain3 = prompt | hf_model | parser
+
+print(f"{hf_model.model_id} output:")
+s = time()
+pprint(chain3.invoke({"query": query}))
+print(f"Time taken: {time() - s} seconds.")
