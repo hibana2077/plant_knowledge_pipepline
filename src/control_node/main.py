371363ac787c6852d1fb2bea10460230fd2938c5
hibana2077 <hibana2077@gmail.com>
2024-03-17 21:10:07 +0800

    Update control_node and data_collector_node main.py files

--- src/control_node/main.py
+++ src/control_node/main.py
@@ -2,18 +2,19 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-03-03 19:30:43
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-03-17 19:42:40
+LastEditTime: 2024-03-17 21:08:06
 FilePath: \plant_knowledge_pipepline\src\control_node\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
+import redis
 from sanic import Sanic
 from sanic.response import text,json
 
-app = Sanic("MyHelloWorldApp")
+app = Sanic("control_node")
 
 @app.get("/")
 async def hello_world(request):
-    return json({"hello": "world"})
+    return json({"stats": "live"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
--- src/data_collector_node/main.py
+++ src/data_collector_node/main.py
@@ -2,10 +2,11 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-03-03 19:29:24
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-03-12 09:40:19
+LastEditTime: 2024-03-17 21:09:52
 FilePath: \plant_knowledge_pipepline\src\data_collection_node\main.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+Description: Scraping data from the web
 '''
+import redis
 from langchain_community.document_loaders import WebBaseLoader
 from pprint import pprint
 
