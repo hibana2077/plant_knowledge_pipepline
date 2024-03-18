5a372630ae3bc2a650a43acbf15dc76d958d99fe
hibana2077 <hibana2077@gmaill.com>
2024-03-18 16:01:03 +0800

    Fix email address typo in LastEditors field

--- src/data_collector_node/main.py
+++ src/data_collector_node/main.py
@@ -1,8 +1,8 @@
 '''
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-03-03 19:29:24
-LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-03-17 21:18:27
+LastEditors: hibana2077 hibana2077@gmaill.com
+LastEditTime: 2024-03-18 15:58:17
 FilePath: \plant_knowledge_pipepline\src\data_collection_node\main.py
 Description: Scraping data from the web
 '''
@@ -12,18 +12,24 @@ import logging
 import time
 from langchain_community.document_loaders import WebBaseLoader
 from pprint import pprint
+from warnings import simplefilter
+from urllib3.exceptions import InsecureRequestWarning
 
 # Connect to redis
 redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = os.getenv("REDIS_PORT", 6379)
 
-client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+# client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-loader = WebBaseLoader("https://www.ptt.cc/bbs/NBA/M.1710203659.A.06B.html")
+# Suppress warnings
+simplefilter(action="ignore", category=FutureWarning)
+simplefilter(action="ignore", category=InsecureRequestWarning)
+
+loader = WebBaseLoader("https://www.mdnkids.com/content.asp?Link_String_=223I00000KHCMHC")
 loader.requests_kwargs = {"verify":False}
 
 data = loader.load()
