7b347b6cfee08dbf5fd901bc934f138752c61dab
hibana2077 <hibana2077@gmail.com>
2024-03-20 14:51:27 +0800

    Add heartbeat interval environment variable

--- src/data_collector_node/main.py
+++ src/data_collector_node/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-03-03 19:29:24
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-03-20 13:33:48
+LastEditTime: 2024-03-20 14:51:20
 FilePath: \plant_knowledge_pipepline\src\data_collection_node\main.py
 Description: Scraping data from the web
 '''
@@ -10,11 +10,15 @@ import redis
 import os
 import logging
 import time
+import json
 from langchain_community.document_loaders import WebBaseLoader
 from pprint import pprint
 from warnings import simplefilter
 from urllib3.exceptions import InsecureRequestWarning
 from time import sleep
+
+# Basic config
+heartbeat_interval = os.getenv("HEARTBEAT_INTERVAL", 60)
 
 # Connect to redis
 redis_host = os.getenv("REDIS_HOST", "localhost")
@@ -47,17 +51,35 @@ simplefilter(action="ignore", category=InsecureRequestWarning)
 #     "timestamp": "2024-03-03 19:29:24",
 # }
 
+def process(job_type:str, url:str, allowed_url_patterns:list):
+    if job_type == "single_page":
+        loader = WebBaseLoader(url)
+        loader.requests_kwargs = {"verify":False}
+        data = loader.load()
+        print(len(data))
+        pprint(data[0].page_content)
+        
 if __name__ == "__main__":
     logger.info("Data collection node active")
     logger.info("Config:")
     logger.info(f"REDIS_HOST: {redis_host}")
     logger.info(f"REDIS_PORT: {redis_port}")
+    heartbeat_reg = time.time()
     while True:
         try:
             # Connect to redis
             client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
             # Check for new Jobs
-            job = client.blpop("data_collection_opens_jobs", 0)
+            job = client.blpop("data_collection_opens_jobs", 10)
+            if job != None:
+                job = json.loads(job[1].decode("utf-8"))
+                logger.info(f"Received job: {job}") if time.time() - heartbeat_reg > heartbeat_interval else None
+                # get job type
+                job_type = job["job_type"]
+                url = job["url"]
+                allowed_url_patterns = job["allowed_url_patterns"]
+                process(job_type, url, allowed_url_patterns)
+            
         except KeyboardInterrupt:
             logger.info("Data collection node shutting down")
             break
