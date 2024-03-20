4d4e1b1267ba2a442583ea9a2b239d8752e441d9
hibana2077 <hibana2077@gmail.com>
2024-03-20 13:33:48 +0800

    Fix bug in login functionality

--- src/data_collector_node/main.py
+++ src/data_collector_node/main.py
@@ -1,8 +1,8 @@
 '''
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-03-03 19:29:24
-LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2024-03-19 14:11:34
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2024-03-20 13:33:48
 FilePath: \plant_knowledge_pipepline\src\data_collection_node\main.py
 Description: Scraping data from the web
 '''
@@ -38,5 +38,32 @@ simplefilter(action="ignore", category=InsecureRequestWarning)
 # print(len(data))
 # pprint(data[0].page_content)
 
+# job structure
+# {
+#     "job_id": "123",
+#     "job_type": "single_page", # single_page, multi_page, sitemap
+#     "url": "https://iastate.pressbooks.pub/quantitativegenetics/",
+#     "allowed_url_patterns": ["https://iastate.pressbooks.pub/quantitativegenetics/"],
+#     "timestamp": "2024-03-03 19:29:24",
+# }
+
 if __name__ == "__main__":
-    logger.info("Data collection node active") 
+    logger.info("Data collection node active")
+    logger.info("Config:")
+    logger.info(f"REDIS_HOST: {redis_host}")
+    logger.info(f"REDIS_PORT: {redis_port}")
+    while True:
+        try:
+            # Connect to redis
+            client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+            # Check for new Jobs
+            job = client.blpop("data_collection_opens_jobs", 0)
+        except KeyboardInterrupt:
+            logger.info("Data collection node shutting down")
+            break
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            break
+        except:
+            logger.error("An unknown error occurred")
+            break
