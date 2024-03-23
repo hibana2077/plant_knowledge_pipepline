843834e44362a7e3e0b41a11ce5ff0089aacbaaf
hibana2077 <hibana2077@gmail.com>
2024-03-23 21:44:04 +0800

    Update get_jobs function to return a list of decoded jobs

--- src/control_node/main.py
+++ src/control_node/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-03-03 19:30:43
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-03-23 21:13:59
+LastEditTime: 2024-03-23 21:43:52
 FilePath: \plant_knowledge_pipepline\src\control_node\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -25,7 +25,10 @@ async def hello_world(request):
 async def get_jobs(request):
     # get redis list without pop
     jobs = redis_client.lrange("data_collection_opens_jobs", 0, -1)
-    return json({"jobs": jobs})
+    return_list = []
+    for job in jobs:
+        return_list.append(job.decode("utf-8"))
+    return json(return_list)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
