a39435843fc385e4f4f1b6cd833ae36c008a2622
hibana2077 <hibana2077@gmail.com>
2024-03-29 22:30:06 +0800

    Add LineChartHero component to dashboard page

---
+++ src/dashboard/src/components/Line_Chart.tsx
@@ -0,0 +1,92 @@
+/*
+ * @Author: hibana2077 hibana2077@gmail.com
+ * @Date: 2024-03-29 22:05:55
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2024-03-29 22:29:13
+ * @FilePath: \plant_knowledge_pipepline\src\dashboard\src\components\Line_Chart.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { LineChart } from '@tremor/react';
+
+// need to connect to database to get the data
+
+const chartdata = [
+  {
+    date: 'Jan 22',
+    SemiAnalysis: 2890,
+    'The Pragmatic Engineer': 2338,
+  },
+  {
+    date: 'Feb 22',
+    SemiAnalysis: 2756,
+    'The Pragmatic Engineer': 2103,
+  },
+  {
+    date: 'Mar 22',
+    SemiAnalysis: 3322,
+    'The Pragmatic Engineer': 2194,
+  },
+  {
+    date: 'Apr 22',
+    SemiAnalysis: 3470,
+    'The Pragmatic Engineer': 2108,
+  },
+  {
+    date: 'May 22',
+    SemiAnalysis: 3475,
+    'The Pragmatic Engineer': 1812,
+  },
+  {
+    date: 'Jun 22',
+    SemiAnalysis: 3129,
+    'The Pragmatic Engineer': 1726,
+  },
+  {
+    date: 'Jul 22',
+    SemiAnalysis: 3490,
+    'The Pragmatic Engineer': 1982,
+  },
+  {
+    date: 'Aug 22',
+    SemiAnalysis: 2903,
+    'The Pragmatic Engineer': 2012,
+  },
+  {
+    date: 'Sep 22',
+    SemiAnalysis: 2643,
+    'The Pragmatic Engineer': 2342,
+  },
+  {
+    date: 'Oct 22',
+    SemiAnalysis: 2837,
+    'The Pragmatic Engineer': 2473,
+  },
+  {
+    date: 'Nov 22',
+    SemiAnalysis: 2954,
+    'The Pragmatic Engineer': 3848,
+  },
+  {
+    date: 'Dec 22',
+    SemiAnalysis: 3239,
+    'The Pragmatic Engineer': 3736,
+  },
+];
+
+const dataFormatter = (number: number | bigint) =>
+  `$${Intl.NumberFormat('us').format(number).toString()}`;
+
+export function LineChartHero() {
+  return (
+    <LineChart
+      className="h-80"
+      data={chartdata}
+      index="date"
+      categories={['SemiAnalysis', 'The Pragmatic Engineer']}
+      colors={['indigo', 'rose']}
+      valueFormatter={dataFormatter}
+      yAxisWidth={60}
+      onValueChange={(v) => console.log(v)}
+    />
+  );
+}
