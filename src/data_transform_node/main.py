9c6e81a4d43bb9c73859b977af059096a7b3a0c9
hibana2077 <hibana2077@gmail.com>
2024-03-30 20:20:13 +0800

    Update main.py and lab.ipynb

--- src/data_transform_node/main.py
+++ src/data_transform_node/main.py
@@ -1,11 +1,3 @@
-'''
-Author: hibana2077 hibana2077@gmail.com
-Date: 2024-03-07 19:42:28
-LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-03-29 21:56:38
-FilePath: \plant_knowledge_pipepline\src\data_transform_node\main.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import WikipediaLoader
 from langchain.text_splitter import TokenTextSplitter
