b9c1beb24c993044bfc3671a63544693dd946538
hibana2077 <hibana2077@gmail.com>
2024-04-11 13:35:46 +0800

    Update favicon.svg, Line_Chart.tsx, and index.astro files

--- src/dashboard/src/components/Line_Chart.tsx
+++ src/dashboard/src/components/Line_Chart.tsx
@@ -2,7 +2,7 @@
  * @Author: hibana2077 hibana2077@gmail.com
  * @Date: 2024-03-29 22:05:55
  * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2024-03-29 22:32:22
+ * @LastEditTime: 2024-04-11 12:04:57
  * @FilePath: \plant_knowledge_pipepline\src\dashboard\src\components\Line_Chart.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,68 +14,68 @@ import { LineChart } from '@tremor/react';
 const chartdata = [
   {
     date: 'Jan 22',
-    SemiAnalysis: 2890,
-    'The Pragmatic Engineer': 2338,
+    'Done Job': 2890,
+    'Queue Job': 2338,
   },
   {
     date: 'Feb 22',
-    SemiAnalysis: 2756,
-    'The Pragmatic Engineer': 2103,
+    'Done Job': 2756,
+    'Queue Job': 2103,
   },
   {
     date: 'Mar 22',
-    SemiAnalysis: 3322,
-    'The Pragmatic Engineer': 2194,
+    'Done Job': 3322,
+    'Queue Job': 2194,
   },
   {
     date: 'Apr 22',
-    SemiAnalysis: 3470,
-    'The Pragmatic Engineer': 2108,
+    'Done Job': 3470,
+    'Queue Job': 2108,
   },
   {
     date: 'May 22',
-    SemiAnalysis: 3475,
-    'The Pragmatic Engineer': 1812,
+    'Done Job': 3475,
+    'Queue Job': 1812,
   },
   {
     date: 'Jun 22',
-    SemiAnalysis: 3129,
-    'The Pragmatic Engineer': 1726,
+    'Done Job': 3129,
+    'Queue Job': 1726,
   },
   {
     date: 'Jul 22',
-    SemiAnalysis: 3490,
-    'The Pragmatic Engineer': 1982,
+    'Done Job': 3490,
+    'Queue Job': 1982,
   },
   {
     date: 'Aug 22',
-    SemiAnalysis: 2903,
-    'The Pragmatic Engineer': 2012,
+    'Done Job': 2903,
+    'Queue Job': 2012,
   },
   {
     date: 'Sep 22',
-    SemiAnalysis: 2643,
-    'The Pragmatic Engineer': 2342,
+    'Done Job': 2643,
+    'Queue Job': 2342,
   },
   {
     date: 'Oct 22',
-    SemiAnalysis: 2837,
-    'The Pragmatic Engineer': 2473,
+    'Done Job': 2837,
+    'Queue Job': 2473,
   },
   {
     date: 'Nov 22',
-    SemiAnalysis: 2954,
-    'The Pragmatic Engineer': 3848,
+    'Done Job': 2954,
+    'Queue Job': 3848,
   },
   {
     date: 'Dec 22',
-    SemiAnalysis: 3239,
-    'The Pragmatic Engineer': 3736,
+    'Done Job': 3239,
+    'Queue Job': 3736,
   },
 ];
 
 const dataFormatter = (number: number | bigint) =>
-  `$${Intl.NumberFormat('us').format(number).toString()}`;
+  `${Intl.NumberFormat('us').format(number).toString()}`;
 
 export function LineChartHero() {
   return (
@@ -83,7 +83,7 @@ export function LineChartHero() {
       className="h-80"
       data={chartdata}
       index="date"
-      categories={['SemiAnalysis', 'The Pragmatic Engineer']}
+      categories={['Done Job', 'Queue Job']}
       colors={['indigo', 'rose']}
       valueFormatter={dataFormatter}
       yAxisWidth={60}
